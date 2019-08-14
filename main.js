88840a087eaf4812cb00d116cd61b3d02a36f201
Senne Berger <sennebergerbc@gmail.com>
2019-08-14 09:23:42 +0200

    done

---
+++ main.js
@@ -0,0 +1,19 @@
+//good page
+function imageOne() {
+  document.getElementById('imageone').style.display = 'none';
+  document.getElementById('imagethree').style.display = 'block';
+  document.getElementById('pyro').style.display = 'block';
+}
+function imageTwo() {
+  document.getElementById('imagetwo').style.display = 'none';
+  document.getElementById('imagefour').style.display = 'block';
+}
+//where page
+function whereOne() {
+  document.getElementById('whereone').style.display = 'none';
+  document.getElementById('wheretwo').style.display = 'block';
+}
+function endOne() {
+  document.getElementById('endtwo').style.display = 'block';
+
+}
